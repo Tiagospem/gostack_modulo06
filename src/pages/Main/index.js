eef445e1d8d11820fd6d7e619b82f0bf221a1b22
Tiago Padilha <tiagospem@gmail.com>
2019-09-01 20:32:31 -0300

    routes and statusbar added

---
+++ src/pages/Main/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function Main() {
+  return (
+    <>
+      <View>
+        <Text>oi</Text>
+      </View>
+    </>
+  );
+}
+
+Main.navigationOptions = {
+  title: 'Main',
+};
